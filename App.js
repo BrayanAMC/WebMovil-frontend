1a204fec202924bbb40d879a332b32494e52e6c9
MarceloAGD <marcelo.guerra@alumnos.ucn.cl>
2023-09-28 21:14:39 -0300

    feat: update app.js

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { AppLoading } from 'expo';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {LoginScreen} from "./src/screens/LoginScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hello</Text>
-      
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const client = new ApolloClient({
+  uri: 'localhost:4000/graphql',
+  cache: new InMemoryCache()
 });
+
+export default function App () {
+  const [loadingCache, setLoadingCache] = useState(true)
+
+  useEffect(() => {
+    persistCache({
+      cache,
+      storage: AsyncStorage,
+    }).then(() => setLoadingCache(false))
+  }, [])
+
+
+  return(
+  <ApolloProvider client={client}>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </ApolloProvider>
+  )
+};
